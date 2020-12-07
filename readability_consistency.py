33e85f92966518d682bee1301837899d6ef7d2cc
ycui1 <yong.cui01@gmail.com>
2020-12-07 14:51:24 -0600

    A tutorial about function arguments

---
+++ readability_consistency.py
@@ -0,0 +1,69 @@
+array0 = [
+    "item_long_var0",
+    "item_long_var1",
+    "item_long_var2"
+]
+
+array1 = [
+    "item_long_var0",
+    "item_long_var1",
+    "item_long_var2"
+]
+
+do_something(param0=0, param1="hello", param2=["1", "2", "3"], param3=[2, 4, 5],
+             param4="Item", param5="Hello World")
+
+do_something(param0=100000, param1="hello world", param2=["1", "2", "3", "4", "5"],
+             param3=[1, 2, 4, 5], param4="Long Item", param5="Hello World")
+
+do_something(param0=0,
+             param1="hello",
+             param2=["1", "2", "3"],
+             param3=[2, 4, 5],
+             param4="Item",
+             param5="Hello World")
+
+do_something(param0=100000,
+             param1="hello world",
+             param2=["1", "2", "3", "4", "5"],
+             param3=[1, 2, 4, 5],
+             param4="Long Item",
+             param5="Hello World")
+
+function getProcessedData() {
+    // 10 lines of code to fetch the data from the server
+    // 5 lines of code to run necessary calculation on the data
+    // 1 or 2 lines of code to format the returned data
+}
+
+function getProcessedData() {
+    fetchDataRemotely()
+    calculateData()
+    // 1 or 2 lines of code to format the returned data
+}
+
+function fetchDataRemotely() {
+    // 10 line of code
+}
+
+function calculateData() {
+    // 5 lines of code
+}
+
+function getProcessedData() {
+    fetchDataRemotely()
+    calculateData()
+    formatData()
+}
+
+function fetchDataRemotely() {
+    // 10 line of code
+}
+
+function calculateData() {
+    // 5 lines of code
+}
+
+function formatData() {
+    // 1 or 2 lines of code
+}
