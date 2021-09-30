6a5b039c643f765e383d6010dfb2e52189d05b0f
ycui1 <yong.cui01@gmail.com>
2021-09-29 23:50:12 -0500

    updated dependencies and locations

--- streamlit_sharing_tutorial/streamlit_sharing.py
+++ streamlit_sharing_tutorial/streamlit_sharing.py
@@ -8,7 +8,7 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
 st.title("Connect to Google Sheets")
-gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/edit?usp=sharing"
+gsheet_url = st.secrets["public_gsheets_url"]
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
 df_gsheet = pd.DataFrame(rows)
